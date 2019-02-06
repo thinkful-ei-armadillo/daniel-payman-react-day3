6df05b9ea150b35f242401c3de753ecfd48a0c6a
Daniel Ors <do7811@gmail.com>
2019-02-06 11:21:08 -0500

    solved helloworld drill

--- react-playground/src/state-drills/HelloWorld.js
+++ react-playground/src/state-drills/HelloWorld.js
@@ -2,42 +2,37 @@ import React from 'react';
 
 class HelloWorld extends React.Component {
   state = {
-    who: 'world'
-};
+      who: 'world'
+  }
 
-handleClickWorld = () => {
-    // State is updated to who: 'world' using setState
-    this.setState({
-        who: 'world'
-    });
-};
+  handleClickWorld = () => {
+    this.setState(
+        {who: 'world'}
+    )  
+  }
 
-handleClickFriend = () => {
-    // State is updated to who: 'friend' using setState
-    this.setState({
-        who: 'friend'
-    });
-};
+  handleClickFriend = () => {
+    this.setState(
+        {who: 'friend'}
+    )  
+  }
 
-handleClickReact = () => {
-    // State is updated too who: 'react' using setState
-    this.setState({
-        who: 'react'
-    });
-};
+  handleClickReact = () => {
+    this.setState(
+        {who: 'react'}
+    )  
+  }
 
 render() {
     return (
         <div className="helloWorld">
-            <button onClick={this.handleClickWorld} className="world-button">World</button>
-            <button onClick={this.handleClickFriend} className="friend-button">Friend</button>
-            <button onClick={this.handleClickReact} className="react-button">React</button>
+            <button onClick = {this.handleClickWorld} className="world">World</button>
+            <button onClick = {this.handleClickFriend} className="friend">Friend</button>
+            <button onClick = {this.handleClickReact} className="react">React</button>
 
-            {/* Conditional rendering: show the p if state.who equals string state */}
-            {this.state.who === 'world' && <p>Hello, world!</p>}
-            {this.state.who === 'friend' && <p>Hello, friend!</p>}
-            {this.state.who === 'react' && <p>Hello, React!</p>}
+            <p>Hello, {this.state.who}!</p>
         </div>
+
     );
   }
 }
